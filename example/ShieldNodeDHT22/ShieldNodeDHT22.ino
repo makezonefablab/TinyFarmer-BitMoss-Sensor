6aaec9f8940e45a3dbf1573c4f8aaf5006962171
MEDIAFLOW-BW\baek <makezone@mediaflow.kr>
2016-11-08 13:43:37 +0900

    Example for Sensors

---
+++ example/ShieldNodeDHT22/ShieldNodeDHT22.ino
@@ -0,0 +1,198 @@
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+
+
+#define HC12_SET          7
+
+#define SERIAL_SPEED 19200
+#define JSON_BUFFER_SIZE              300   /* JSON 버퍼 사이즈 */
+
+#define DHTPIN 4       /*온습도 센서 핀 번호*/
+#define DHTTYPE DHT22   /*온습도 센서 타입*/
+
+
+int nodeID = 1;                            /* 메시 네트워크 Node 아이디, 스위치로부터 해당 값을 새로 설정 함 */
+int channel = 9;                            /* 기본 채널 1 */
+
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+String recivedData = "";
+
+struct sensor_data_t {                      /* 센서 데이터 Struct: 메시 네트워크 내부에서만 사용 */
+  int id;
+  float temp;
+  float hum;
+  long ill;
+  int co2;
+  float ph;
+  float ec;
+  float soil_temp;
+  float soil_hum;
+  int wind_dir;
+  float wind_vol;
+  float rainfall;
+  bool isBooted;
+};
+
+sensor_data_t sensorDataT;                            /* 센서 데이터 전송을 위한 센서 데이터 Struct 선언 */
+
+DHT dht(DHTPIN, DHTTYPE);                             /* 온습도 센서 데이터 선언 */
+
+void setup()
+{
+
+  // 시리얼 셋팅
+  Serial.begin(19200);
+
+  String chString = "AT+C00" + String(channel) + "\r\n";
+
+
+  // 초기화 (HC-12)
+  pinMode(HC12_SET, OUTPUT);
+  digitalWrite(HC12_SET, LOW);  // AT 설정 모드 진입
+
+  Serial.println(chString);    // channel setting
+
+
+  delay(100);
+  digitalWrite(HC12_SET, HIGH); // AT 설정 모드 빠져나옴
+  Serial.println("HC12 Init Done" );
+
+
+  // 센서 데이터 초기화
+  sensorDataT.id = nodeID;
+  sensorDataT.ph = 0;
+  sensorDataT.ec = 0;
+  sensorDataT.soil_temp = 0;
+  sensorDataT.soil_hum = 0;
+  sensorDataT.ill = 0;
+  sensorDataT.co2 = 0;
+  sensorDataT.wind_dir = 0;
+  sensorDataT.wind_vol = 0;
+  sensorDataT.rainfall = 0;
+  sensorDataT.isBooted = true;
+
+  delay(100);
+
+  // 온습도 센서 작동 시작
+  Serial.println("DHT22 Init Done!");
+  dht.begin();
+
+  delay(3000);
+
+}
+
+
+void loop()
+{
+  // Power PIN Select에 신호를 ON하여 센서와 RF에 전원 공급
+  delay(1000);
+
+  //////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // 센서 값을 읽어 메시 네트워크로 전송
+  //  : 마스터가 해당 ID로 요청
+  //  : 요청된 ID 센서노드만 데이터 전송
+  //
+  //  {"ch":"00","id":"00","req":"1"}
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////
+  if (stringComplete == true)
+  {
+
+    //Serial.println("parsing recivedData : " + recivedData);
+
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject(recivedData);
+
+    //Serial.println("parsing Jason");
+
+    String ch = "";
+    String id = "";
+    String req = "";
+
+    // {"ch":"ddd","id":"ddd","req":"2222"}
+
+    if (root.success())
+    {
+      String _ch = root["ch"];
+      String _id = root["id"];
+      String _req = root["req"];
+
+      ch = _ch;
+      id = _id;
+      req = _req;
+
+      //Serial.println("parsing completed");
+    }
+
+    // 수신된 ID와 센서 ID 비교
+    if (id == String(nodeID) && req == "1")
+    {
+      readSensorValue();
+
+      /*
+         JSON
+
+         {"id":"00", "temp":"00", "hum":"00", "ill":"00", "co2":"00", "ph":"00", "ec":"00", "soil_temp":"00", "soil_hum":"00", "wind_dir":"00", "id":"rainfall", "isBooted":"1", "sleep":"00"}
+
+      */
+
+      StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
+      JsonObject& root = jsonBuffer.createObject();
+      root["id"] = String(nodeID);
+      root["temp"] = sensorDataT.temp;
+      root["hum"] = sensorDataT.hum;
+      //        root["ill"] = sensorDataT.ill;
+      //        root["co2"] = sensorDataT.co2;
+      //        root["ph"] = sensorDataT.ph;
+
+      //        root["ec"] = sensorDataT.ec;
+      //        root["soil_temp"] = sensorDataT.soil_temp;
+      //        root["soil_hum"] = sensorDataT.soil_hum;
+      //        root["wind_dir"] = sensorDataT.wind_dir;
+      //        root["rainfall"] = sensorDataT.rainfall;
+      //        root["isBooted"] = sensorDataT.isBooted;
+      //        root["sleep"] = "";
+      root.printTo(Serial);
+
+    }
+    stringComplete = false;
+  }
+}
+
+//센서 데이터 읽기
+void readSensorValue()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // Temperature & Humidity sensor value
+  if (isnan(h) || isnan(t)) {
+    //Serial.println("Failed to read from DHT using previous data.");
+  }
+  else {
+    sensorDataT.temp = t;
+    sensorDataT.hum = h;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+
+    // get the new byte:
+    char inChar = (char)Serial.read();
+
+    // add it to the inputString:
+    inputString += inChar;
+
+    if (inChar == '}') {
+      recivedData = inputString;
+      inputString = "";
+      stringComplete = true;
+    }
+  }
+}
+
---
+++ example/ShieldNodeWeather/ShieldNodeWeather.ino
@@ -0,0 +1,236 @@
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+
+#define HC12_SET          7
+
+#define SERIAL_SPEED 19200
+#define JSON_BUFFER_SIZE              300   /* JSON 버퍼 사이즈 */
+
+// 센서 SW 시리얼
+#define RX                 6      /* RX 핀 번호 */
+#define TX                 5      /* TX 핀 번호 */
+
+SoftwareSerial mySerial(RX, TX);
+
+char databuffer[35];                        /* 시리얼 데이터 읽기 위한 버퍼 */
+
+int nodeID = 1;                            /* 메시 네트워크 Node 아이디, 스위치로부터 해당 값을 새로 설정 함 */
+int channel = 9;                            /* 기본 채널 1 */
+
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+String recivedData = "";
+
+struct sensor_data_t {                      /* 센서 데이터 Struct: 메시 네트워크 내부에서만 사용 */
+  int id;
+  float temp;
+  float hum;
+  long ill;
+  int co2;
+  float ph;
+  float ec;
+  float soil_temp;
+  float soil_hum;
+  int wind_dir;
+  float wind_vol;
+  float rainfall;
+  bool isBooted;
+};
+
+sensor_data_t sensorDataT;                            /* 센서 데이터 전송을 위한 센서 데이터 Struct 선언 */
+
+
+void setup()
+{
+
+  // 시리얼 셋팅
+  Serial.begin(19200);
+  mySerial.begin(9600);
+  String chString = "AT+C00" + String(channel) + "\r\n";
+
+
+  // 초기화 (HC-12)
+  pinMode(HC12_SET, OUTPUT);
+  digitalWrite(HC12_SET, LOW);  // AT 설정 모드 진입
+
+  Serial.println(chString);    // channel setting
+
+
+  delay(100);
+  digitalWrite(HC12_SET, HIGH); // AT 설정 모드 빠져나옴
+  Serial.println("HC12 Init Done" );
+
+
+  // 센서 데이터 초기화
+  sensorDataT.id = nodeID;
+  sensorDataT.ph = 0;
+  sensorDataT.ec = 0;
+  sensorDataT.soil_temp = 0;
+  sensorDataT.soil_hum = 0;
+  sensorDataT.ill = 0;
+  sensorDataT.co2 = 0;
+  sensorDataT.wind_dir = 0;
+  sensorDataT.wind_vol = 0;
+  sensorDataT.rainfall = 0;
+  sensorDataT.isBooted = true;
+
+  delay(3000);
+}
+
+
+void loop()
+{
+  // Power PIN Select에 신호를 ON하여 센서와 RF에 전원 공급
+  delay(1000);
+
+  //////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // 센서 값을 읽어 메시 네트워크로 전송
+  //  : 마스터가 해당 ID로 요청
+  //  : 요청된 ID 센서노드만 데이터 전송
+  //
+  //  {"ch":"00","id":"00","req":"1"}
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////
+  if (stringComplete == true)
+  {
+    //Serial.println("parsing recivedData : " + recivedData);
+
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject(recivedData);
+
+    //Serial.println("parsing Jason");
+
+    String ch = "";
+    String id = "";
+    String req = "";
+
+    // {"ch":"ddd","id":"ddd","req":"2222"}
+
+    if (root.success())
+    {
+      String _ch = root["ch"];
+      String _id = root["id"];
+      String _req = root["req"];
+
+      ch = _ch;
+      id = _id;
+      req = _req;
+
+      //Serial.println("parsing completed");
+    }
+
+    // 수신된 ID와 센서 ID 비교
+    if (id == String(nodeID) && req == "1")
+    {
+      readSensorValue();
+
+      /*
+         JSON
+
+         {"id":"00", "temp":"00", "hum":"00", "ill":"00", "co2":"00", "ph":"00", "ec":"00", "soil_temp":"00", "soil_hum":"00", "wind_dir":"00", "id":"rainfall", "isBooted":"1", "sleep":"00"}
+
+      */
+
+      StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
+      JsonObject& root = jsonBuffer.createObject();
+      root["id"] = String(nodeID);
+      //        root["temp"] = sensorDataT.temp;
+      //        root["hum"] = sensorDataT.hum;
+      //        root["ill"] = sensorDataT.ill;
+      //        root["co2"] = sensorDataT.co2;
+      //        root["ph"] = sensorDataT.ph;
+
+      //        root["ec"] = sensorDataT.ec;
+      //        root["soil_temp"] = sensorDataT.soil_temp;
+      //        root["soil_hum"] = sensorDataT.soil_hum;
+      root["wind_dir"] = sensorDataT.wind_dir;
+      root["rainfall"] = sensorDataT.rainfall;
+      //        root["isBooted"] = sensorDataT.isBooted;
+      //        root["sleep"] = "";
+      root.printTo(Serial);
+
+    }
+    stringComplete = false;
+  }
+}
+
+// Weather (SoftwareSerial)
+float transCharToIntWeather(char *_buffer, int _start, int _stop)
+{
+  int _index;
+  float result = 0;
+  int num = _stop - _start + 1;
+  int _temp[num];
+  for (_index = _start; _index <= _stop; _index ++)
+  {
+    _temp[_index - _start] = _buffer[_index] - '0';
+    result = 10 * result + _temp[_index - _start];
+  }
+  return result;
+}
+
+int WindDirection()                                                                  //Wind Direction
+{
+  return transCharToIntWeather(databuffer, 1, 3);
+}
+
+
+float WindSpeedAverage()                                                             //air Speed (1 minute)
+{
+  return 0.44704 * transCharToIntWeather(databuffer, 5, 7);
+}
+
+float RainfallOneHour()                                                              //Rainfall (1 hour)
+{
+  return transCharToIntWeather(databuffer, 17, 19) * 25.40 * 0.01;
+}
+
+//센서 데이터 읽기
+void readSensorValue()
+{
+  
+  // read
+    int index;
+    for (index = 0; index < 35; index ++)
+    {
+      
+      if (mySerial.available())
+      {
+        databuffer[index] = mySerial.read();
+        if (databuffer[0] != 'c')
+        {
+          index = -1;
+        }
+      }
+      else
+      {
+        index --;
+      }
+    }
+
+    //save
+    sensorDataT.wind_dir = WindDirection();
+    sensorDataT.wind_vol = WindSpeedAverage();
+    sensorDataT.rainfall = RainfallOneHour();
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+
+    // get the new byte:
+    char inChar = (char)Serial.read();
+
+    // add it to the inputString:
+    inputString += inChar;
+
+    if (inChar == '}') {
+      recivedData = inputString;
+      inputString = "";
+      stringComplete = true;
+    }
+  }
+}
+
